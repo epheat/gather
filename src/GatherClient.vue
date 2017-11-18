7d5c5cb949629975ac66669245f9df4888383f76
ParkerH96 <parker.a.householder@gmail.com>
2017-11-18 18:38:03 -0500

    Fixed something

--- src/GatherClient.vue
+++ src/GatherClient.vue
@@ -29,7 +29,7 @@
               <option value=6>6</option>
             </select>
             <br>
-            <button @click="newGame(index)">New Game</button>
+            <button class="button-style-1" @click="newGame(index)">New Game</button>
           </div>
         </div>
       </div>
